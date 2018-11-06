3ca064a332f0a96fb195d8ddf052af8ba0860099
Manuel <38227219+Tindergarten@users.noreply.github.com>
2018-11-06 12:09:41 +0100

    added german translation

---
+++ locales/de.lua
@@ -0,0 +1,11 @@
+Locales ['de'] = {
+	['tattoo_shop_nearby'] = 'Drücke ~INPUT_PICKUP~ um das ~y~Tattoostudio~s~ zu betreten.',
+	['money_amount']       = '<span style="color:green;">$%s</span>',
+	['part']               = 'Teil %s',
+	['go_back_to_menu']    = '< Zurück',
+	['tattoo_item']        = 'Tattoo %s - %s',
+	['tattoos']            = 'tattoos',
+	['tattoo_shop']        = 'Tattoostudio',
+	['bought_tattoo']      = 'Du hast ein ~y~Tattoo~s~ für ~r~$%s~s~ gekauft',
+	['not_enough_money']   = 'Du hast nicht ~r~genug Geld~s~ für dieses Tattoo! Dir fehlen ~r~$%s~s~'
+}
