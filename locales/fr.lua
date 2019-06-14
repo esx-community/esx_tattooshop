af7f8539094a0654ad0d12cdc8881511486f7dae
Dolu Tattoo <47056777+dolutattoo@users.noreply.github.com>
2019-06-14 19:52:59 +0200

    fix about lowercase

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,11 +1,11 @@
 Locales['fr'] = {
-  ['tattoo_shop_prompt'] = 'Appuyer sur ~INPUT_PICKUP~ pour accéder au ~y~salon de tatouage~s~.',
+  ['tattoo_shop_prompt'] = 'appuyer sur ~INPUT_PICKUP~ pour accéder au ~y~salon de tatouage~s~.',
   ['money_amount']       = '<span style="color:green;">$%s</span>',
   ['part']               = 'partie %s',
-  ['go_back_to_menu']    = '< Retour',
+  ['go_back_to_menu']    = '< retour',
   ['tattoo_item']        = 'tatouage %s - %s',
   ['tattoos']            = 'tatouages',
-  ['tattoo_shop']        = 'Salon de tatouage',
-  ['bought_tattoo']      = 'Votre nouveau ~y~tatouage~s~ vous a couté ~r~$%s~s~',
-  ['not_enough_money']   = 'Vous n\'avez pas ~r~assez d\'argent~s~ pour ce tatouage! Il vous manque ~r~$%s~s~'
+  ['tattoo_shop']        = 'salon de tatouage',
+  ['bought_tattoo']      = 'votre nouveau ~y~tatouage~s~ vous a couté ~r~$%s~s~',
+  ['not_enough_money']   = 'vous n\'avez pas ~r~assez d\'argent~s~ pour ce tatouage! Il vous manque ~r~$%s~s~'
 }
